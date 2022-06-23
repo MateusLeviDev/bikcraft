6400c77a714d49e60c393899cc2543de1c8332ad
mateusleviDev <mateuslevisouzapereira@gmail.com>
2022-06-23 17:25:20 -0300

    JavaScript

--- js/formulario.js
+++ js/formulario.js
@@ -3,6 +3,9 @@ const formulario = document.querySelector('form');
 function enviarFormulario(event) {
     // Vai ter um event dentro, mas iremos previnir o padrão, ou seja, não quero que vá para o arquivo enviar.php
     event.preventDefault();
+    const botao = document.querySelector('form botao'); 
+    botao.disabled = true; 
+    botao.innerText = "Enviando...";
 }
 
 formulario.addEventListener('submit', enviarFormulario)
